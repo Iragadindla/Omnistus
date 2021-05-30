eb8f99a2f9bbc3952d7e94a48b6b0fa0235f3dca
hp <hp@DESKTOP-Q6F9PJA>
2021-05-30 11:03:11 +0530

    Sumo Commit

---
+++ Omnistus/src/test/java/com/OmistusCrm/Operation/Sumo.java
@@ -0,0 +1,13 @@
+package com.OmistusCrm.Operation;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Sumo {
+	@Test
+	public void run() 
+	{
+		Reporter.log(" Demotestcases running sucussfulluy", true);
+	}
+
+}
